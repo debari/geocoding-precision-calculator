c3a78cbbc7730bc8cfd4bb07c8731771ef6179d6
Junya Debari <junya.debari@gmail.com>
2015-10-27 01:49:49 +0900

    リスト全部BigQueryに一括投入する関数を追加

--- bq.py
+++ bq.py
@@ -25,6 +25,10 @@ class BqQuery(object):
             cls._bigquery = build('bigquery', 'v2', http=http)
         return cls._bigquery
 
+    @classmethod
+    def tabledata(cls):
+        return cls.bigquery().tabledata()
+
     def insert_id(self):
         return '{}{}'.format(
             datetime.datetime.now().strftime("%Y%m%d%H%M%S"),
@@ -38,8 +42,14 @@ class BqQuery(object):
                 'json': data
             }
 
+    def build_insert_data(self, data_list):
+        return [{
+            'insertId': self.insert_id(),
+            'json': data
+        } for data in data_list]
+
     def insert(self, data_list):
-        tabledata = self.bigquery().tabledata()
+        tabledata = self.tabledata()
         for insert_data in self.iter_insert_data(data_list):
             try:
                 tabledata.insertAll(
@@ -53,3 +63,18 @@ class BqQuery(object):
                 ).execute()
             except:
                 logging.exception('big query error')
+
+    def insert_all(self, data_list):
+        tabledata = self.tabledata()
+        try:
+            tabledata.insertAll(
+                projectId=self.project_id,
+                datasetId=self.dataset_id,
+                tableId=self.table_id,
+                body={
+                    'kind': 'bigquery#tableDataInsertAllRequest',
+                    'rows': self.build_insert_data(data_list),
+                }
+            ).execute()
+        except:
+            logging.exception('big query error')
