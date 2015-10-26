eb880def986fae453a7bcbbff9080ecb346555af
Junya Debari <junya.debari@gmail.com>
2015-10-27 01:37:38 +0900

    ジオコーディング関係クラスの切り出し

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ import csv
 import webapp2
 import jinja2
 import os
-import json
-from google.appengine.api import urlfetch
 
 from bq import BqQuery
+from geocoding import (
+    GeoCoder,
+    GeoCodingResult,
+)
 from models import (
     Place,
     GeoCodedPlace,
@@ -33,95 +35,8 @@ TABLE_ID = 'places'
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
-
-
-class GeoCoder(object):
-    base = 'https://maps.googleapis.com/maps/api/geocode/json'
-
-    def __init__(self, lat, lng):
-        self.lat = lat
-        self.lng = lng
-        self._result = None
-
-    def reset(self):
-        self.lat = None
-        self.lng = None
-        self._result = None
-
-    def latlng(self):
-        return '{lat},{lng}'.format(lat=self.lat, lng=self.lng)
-
-    def build(self):
-        yield 'latlng', self.latlng()
-        yield 'language', 'ja'
-
-    def query(self):
-        return '&'.join(['{}={}'.format(k, v) for k, v in self.build()])
-
-    def as_qs(self):
-        return '{b}?{q}'.format(b=self.base, q=self.query())
-
-    def fetch(self):
-        res = urlfetch.fetch(self.as_qs())
-        if res.status_code == 200:
-            return json.loads(res.content)
-        return None
-
-    def result(self):
-        if self._result is None:
-            self._result = self.fetch()
-        return self._result
-
-
-class GeoCodingResult(object):
-
-    def __init__(self, geocoder):
-        self.geocoder = geocoder
-
-    def json(self):
-        return self.geocoder.result()['results']
-
-    def result(self):
-        try:
-            return self.json()[0]
-        except KeyError:
-            return {}
-
-    def address(self):
-        return self.result().get('formatted_address', '')
-
-    def geometry(self):
-        return self.result().get('geometry', {})
-
-    def location(self):
-        return self.geometry().get('location', {})
-
-    def lat(self):
-        return self.location().get('lat')
-
-    def lng(self):
-        return self.location().get('lng')
-
-    def type(self):
-        return self.geometry().get('location_type', '')
-
-    def as_dict(self):
-        return {
-            'address': self.address(),
-            'lat': self.lat(),
-            'lng': self.lng(),
-            'json': self.json(),
-        }
-
-    def as_bq(self):
-        return {
-            'address': self.address(),
-            'lat': self.lat(),
-            'lng': self.lng(),
-            'lat_origin': self.geocoder.lat,
-            'lng_origin': self.geocoder.lng,
-        }
+    autoescape=True
+)
 
 
 class MainHandler(webapp2.RequestHandler):
